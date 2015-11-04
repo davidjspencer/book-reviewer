b5066397d29e22072340d4fd8b62b11beae349a2
davidjspencer <davidjspencer@gmail.com>
2015-11-04 17:27:22 +0100

    initial basic book reviewer. Books and notes

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  root to: "books#index" 
+
+  resources :books do
+    resources :notes, only: [:create, :destroy]
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+Book.destroy_all
+
+Book.create! [
+  { name: "Eloquent Ruby", author: "Russ Olsen" },
+  { name: "Beginning Ruby", author: "Peter Cooper" },
+  { name: "Metaprogramming Ruby 2", author: "Paolo Perrotta" },
+  { name: "Design Patterns in Ruby", author: "Russ Olsen" },
+  { name: "The Ruby Programming Language", author: "David Flanagan" }
+]
+
+Note.destroy_all
+
+eloquent = Book.find_by name: "Eloquent Ruby"
+eloquent.notes.create! [
+  { title: "Wow", note: "Great book to learn Ruby" },
+  { title: "Yeah", note: "What she said"}
+]
